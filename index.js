8f40fd0ad4afd4dbe69e3eb244b8fc9602fb3a27
Nia K <nsjkappen@gmail.com>
2021-07-24 10:42:16 -0700

    commit tree e36372f344d767b5449d34d86a4c374dd9c32e33

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 // Code your solutions in this file
 
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   } 
 
-function writeCards(names, event){
-    let messages = [];
-    for(let i = 0; i < names.length; i++){
-        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
-    }
 
-    return messages;
+// function writeCards(names, event){
+//     let messages = [];
+//     for(let i = 0; i < names.length; i++){
+//         messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+//     }
+
+//     return messages;
+// }
+
+
+// function countDown(positiveNumber){
+//     while(positiveNumber >= 0){
+//         console.log(positiveNumber);
+//         positiveNumber--;
+
+
+//     }
+// }
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
 }
 
-
-function countDown(positiveNumber){
-    while(positiveNumber >= 0){
-        console.log(positiveNumber);
-        positiveNumber--;
-
-
-    }
-}
+wrapGifts(gifts); 
